c47c67424b9235035e7e9dbf2448aa622af2aa78
Harsh-Koundal <aharsh33039@gmail.com>
2025-11-20 06:12:10 -0800

    DSA-Q21

---
+++ DSA-Q21.js
@@ -0,0 +1,8 @@
+/**Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well. */
+
+function mySqrt(x){
+return x = Math.floor(Math.sqrt(x));
+// return x = x**0.5;
+}
+
+console.log(mySqrt(36));
