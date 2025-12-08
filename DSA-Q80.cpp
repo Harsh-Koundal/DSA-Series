6246ee24f25bd25324fb78988813ac924e643527
Harsh-Koundal <aharsh33039@gmail.com>
2025-12-08 07:12:49 -0800

    DSA-Q80

---
+++ DSA-Q80.cpp
@@ -0,0 +1,24 @@
+// Problem : Max Consecutive Ones
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxConsecutive(vector<int>& nums){
+    int count=0,maxCount=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==1){
+            count++;
+            maxCount = max(maxCount,count);
+        }else{
+            count=0;
+        }
+    }
+    return maxCount;
+}
+
+int main(){
+    vector<int> nums = {1,1,0,1,1,1};
+    cout << maxConsecutive(nums);
+    return 0;
+}
