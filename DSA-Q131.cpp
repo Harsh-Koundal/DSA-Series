f1dd0dc6ef9c9254cdffb39a3bd8fbea1c8bbd37
Harsh-Koundal <aharsh33039@gmail.com>
2025-12-31 09:06:09 -0800

    Longest Common Prefix

---
+++ DSA-Q131.cpp
@@ -0,0 +1,26 @@
+// Longest Common Prefix
+
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs){
+    if(strs.size()==0) return "";
+    string prefix = strs[0];
+    for(int i=1;i<strs.size();i++){
+        while(strs[i].find(prefix)!=0){
+            prefix = prefix.substr(0,prefix.length()-1);
+        }
+    }
+    return prefix;
+}
+
+int main(){
+    vector<string> strs = {"flower","flow","flight"};
+    cout << longestCommonPrefix(strs);
+    return 0;
+}
