1cb7347776e46ff9db5ffe9b21931abc2bf8c585
Harsh-Koundal <aharsh33039@gmail.com>
2026-01-18 07:36:39 -0800

    Convert 1D Array Into 2D Array

---
+++ DSA-Q183.cpp
@@ -0,0 +1,41 @@
+// Convert 1D Array Into 2D Array
+
+/*
+You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
+
+The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+
+Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> construct2DArray(vector<int>& original,int m,int n){
+    if(original.size() != m*n){
+        return {};
+    }
+
+    vector<vector<int>> res(m,vector<int>(n));
+    int idx = 0;
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            res[i][j] = original[idx++];
+        }
+    }
+    return res;
+}
+
+int main(){
+    vector<int> original = {1,2,3,4};
+    int m=2,n=2;
+    vector<vector<int>> res = construct2DArray(original,m,n);
+    for (auto row : res) {
+        for (auto val : row) {
+            cout << val << " ";
+        }
+    }
+    return 0;
+}
